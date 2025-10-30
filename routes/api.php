5245fb714c97a1218e6749c7a3e777a0bfc2adc4
kheanggg <mengkheang1202@gmail.com>
2025-10-30 23:46:08 +0700

    Notify User & Shop Owner

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\VehicleTypeController;
 use App\Http\Controllers\Api\VehicleController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\TelegramController;
+use App\Http\Controllers\Api\BookingController;
 
 // Locations
 Route::get('/locations', [LocationController::class, 'index']);
@@ -22,3 +23,6 @@ Route::get('/users/{telegramId}', [UserController::class, 'show']);
 
 // Telegram webhook routes
 Route::post('/telegram/webhook', [TelegramController::class, 'handle']);
+
+// Booking notifications
+Route::post('/booking/notify', [BookingController::class, 'sendBookingNotification']);
